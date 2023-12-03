6b1085c7ef66e358c3c7a48c2df715a47544b787
4chernov4 <yarchernov@gmail.com>
2023-12-03 23:36:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class1 = int(input("Введите количество учеников в первом классе: "))
+class2 = int(input("Введите количество учеников во втором классе: "))
+class3 = int(input("Введите количество учеников в третьем классе: "))
+
+class1 = (class1 + 1) // 2
+class2 = (class2 + 1) // 2
+class3 = (class3 + 1) // 2
+
+total_desks = class1 + class2 + class3
+
+print(f"Всего нужно закупить {total_desks} парты для всех учеников.")
